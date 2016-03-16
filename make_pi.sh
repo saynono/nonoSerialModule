fe2b2dc730f88bb3173c4a456594081f33d7164c
nono <say.nono@gmail.com>
2016-03-16 16:32:53 +0000

    Changes

--- make_pi.sh
+++ make_pi.sh
@@ -5,9 +5,10 @@ projectFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $projectFolder
 mkdir tmp
 
+g++ -c -o ./tmp/perlin.o ./../PerlinNoise/PerlinNoise.cpp -Wall -pedantic;
 g++ -c -o ./tmp/nonoSerial.o ./src/nonoSerial.cpp -Wall -pedantic;
 g++ -c -o ./tmp/nonoSerialModule.o ./src/nonoSerialModule.cpp  -Wall -pedantic;
 g++ -c -o ./tmp/nonoSerialModule_UDP_Wrapper.o ./src/nonoSerialModule_UDP_Wrapper.cpp -Wall -pedantic;
-g++ ./tmp/nonoSerialModule.o ./tmp/nonoSerialModule_UDP_Wrapper.o ./tmp/nonoSerial.o -o ./bin/$OUTPUTNAME -lm -Wall -pedantic; 
+g++ ./tmp/perlin.o ./tmp/nonoSerialModule.o ./tmp/nonoSerialModule_UDP_Wrapper.o ./tmp/nonoSerial.o -o ./bin/$OUTPUTNAME -lm -Wall -pedantic; 
 
 rm -rf ./tmp
